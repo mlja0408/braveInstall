54b3b393e83fdb8a2d8980998b4d44cc307a0f78
mlja0408 <80475650+mlja0408@users.noreply.github.com>
2022-11-01 13:54:10 -0700

    Added the scripts provided for other distros, and updated README.md

---
+++ OpenSUSE/braveInstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo zypper install curl
+
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+
+sudo zypper addrepo --refresh https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave-browser
+
+sudo zypper install brave-browser
